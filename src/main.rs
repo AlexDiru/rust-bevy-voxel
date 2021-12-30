044874c68969a910a000db49e550457de00d9a2e
Alex Spedding <alex@alexs-mbp-16.home>
2021-12-30 14:07:43 +0000

    Inefficient, basic, optimal mesh creation for the Maze. Still, its a basic 2d voxel system

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,121 @@ mod point;
 use std::f32::consts::PI;
 use bevy::prelude::*;
 use bevy::render::camera::PerspectiveProjection;
+use bevy::render::pipeline::PrimitiveTopology;
+use bevy::render::RenderPlugin;
+use bevy::render::texture::TextureViewDimension::Cube;
+use bevy::render::wireframe::{Wireframe, WireframePlugin};
+use bevy::wgpu::{WgpuFeature, WgpuFeatures, WgpuOptions};
 use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
 use crate::map::Map;
+use crate::point::Point;
+
+fn top_plane_vertices(x_offset: f32, y_offset: f32, z_offset: f32) -> [([f32; 3], [f32; 3], [f32; 2]); 6] {
+    [
+        ([0.0 + x_offset, 1.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 1.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 1.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+    ]
+}
+
+fn left_plane_vertices(x_offset: f32, y_offset: f32, z_offset: f32) -> [([f32; 3], [f32; 3], [f32; 2]); 6] {
+    [
+        ([0.0 + x_offset, 0.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 1.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 1.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 0.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 1.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 0.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+    ]
+}
+
+fn right_plane_vertices(x_offset: f32, y_offset: f32, z_offset: f32) -> [([f32; 3], [f32; 3], [f32; 2]); 6] {
+    [
+        ([1.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 0.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 0.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 0.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+    ]
+}
+
+fn bottom_plane_vertices() -> [([f32; 3], [f32; 3], [f32; 2]); 6] {
+    [
+        ([1.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
+    ]
+}
+
+fn back_plane_vertices(x_offset: f32, y_offset: f32, z_offset: f32) -> [([f32; 3], [f32; 3], [f32; 2]); 6] {
+    [
+        ([1.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 0.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 0.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 0.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([1.0 + x_offset, 1.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+    ]
+}
+
+fn front_plane_vertices(x_offset: f32, y_offset: f32, z_offset: f32) -> [([f32; 3], [f32; 3], [f32; 2]); 6] {
+    [
+        ([0.0 + x_offset, 0.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 0.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 1.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 0.0 + y_offset, 0.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+        ([0.0 + x_offset, 1.0 + y_offset, 1.0 + z_offset], [0.0, 1.0, 0.0], [1.0, 1.0]),
+    ]
+}
+
+fn generate_mesh(points: &Vec<Point>) -> Mesh {
+    let mut vertices = Vec::new();
+    for (_, point) in points.iter().enumerate() {
+        vertices.extend_from_slice(&top_plane_vertices(point.x as f32, 0.0, point.y as f32));
+
+        // if up point doesn't exist, render back wall
+        if !points.contains(&point.right()) {
+            vertices.extend_from_slice(&back_plane_vertices(point.x as f32, 0.0, point.y as f32));
+        }
+
+        if !points.contains(&point.left()) {
+            vertices.extend_from_slice(&front_plane_vertices(point.x as f32, 0.0, point.y as f32));
+        }
+
+        if !points.contains(&point.down()) {
+            vertices.extend_from_slice(&left_plane_vertices(point.x as f32, 0.0, point.y as f32));
+        }
+
+        if !points.contains(&point.up()) {
+            vertices.extend_from_slice(&right_plane_vertices(point.x as f32, 0.0, point.y as f32));
+        }
+
+    }
+
+    let mut positions = Vec::new();
+    let mut normals = Vec::new();
+    let mut uvs = Vec::new();
+    for (position, normal, uv) in vertices.iter() {
+        positions.push(*position);
+        normals.push(*normal);
+        uvs.push(*uv);
+    }
+
+    let mut mesh = Mesh::new(bevy::render::pipeline::PrimitiveTopology::TriangleList);
+    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+
+    mesh
+}
 
 fn init(
     mut commands: Commands,
@@ -17,38 +130,33 @@ fn init(
         ..Default::default()
     });
 
-    let box_mesh = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
+    let map = Map::dfs_maze();
+    let wall_mesh = meshes.add(generate_mesh(map.get_walls()));
+    let floor_mesh = meshes.add(generate_mesh(map.get_floors()));
+
+    let indices = bevy::render::mesh::Indices::U32(vec![0, 2, 1, 0, 3, 2]);
+
     let wall_material = materials.add(Color::rgb(1.0, 0.2, 0.3).into());
     let floor_material = materials.add(Color::rgb(0.1, 0.7, 0.3).into());
 
-    let map = Map::dfs_maze();
+    commands.spawn().insert_bundle(PbrBundle {
+        mesh: wall_mesh.clone(),
+        material: wall_material.clone(),
+        transform: Transform::from_translation(Vec3::ZERO),
+        ..Default::default()
+    }).insert(Wireframe);
 
-    for (_, wall) in map.get_walls().iter().enumerate() {
-        commands.spawn().insert_bundle(PbrBundle {
-            mesh: box_mesh.clone(),
-            material: wall_material.clone(),
-            transform: Transform::from_translation(Vec3::new(
-                wall.x as f32, 0.0, wall.y as f32,
-            )),
-            ..Default::default()
-        });
-    }
-
-    for (_, floor) in map.get_floors().iter().enumerate() {
-        commands.spawn().insert_bundle(PbrBundle {
-            mesh: box_mesh.clone(),
-            material: floor_material.clone(),
-            transform: Transform::from_translation(Vec3::new(
-                floor.x as f32, -1.0, floor.y as f32,
-            )),
-            ..Default::default()
-        });
-    }
+    commands.spawn().insert_bundle(PbrBundle {
+        mesh: floor_mesh.clone(),
+        material: floor_material.clone(),
+        transform: Transform::from_xyz(0.0, -1.0, 0.0),
+        ..Default::default()
+    });
 
     commands
         .spawn()
         .insert_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(map.get_start().x as f32, 0.25, map.get_start().y as f32),
+            transform: Transform::from_xyz(map.get_start().x as f32 + 0.5, 0.25, map.get_start().y as f32 + 0.5),
             perspective_projection: PerspectiveProjection {
                 near: 0.01,
                 ..Default::default()
@@ -80,7 +188,14 @@ fn toggle_button_system(
 fn main() {
     App::build()
         .insert_resource(Msaa { samples: 4 })
+        .insert_resource(WgpuOptions {
+            features: WgpuFeatures {
+                features: vec![WgpuFeature::NonFillPolygonMode]
+            },
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugin(WireframePlugin)
         .add_startup_system(init.system())
         .add_plugin(FlyCameraPlugin)
         .add_system(toggle_button_system.system())
